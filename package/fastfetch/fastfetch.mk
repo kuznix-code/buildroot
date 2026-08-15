47b5388933175c19e1e002520c9d015066fa220e
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-08-15 22:06:31 +0200

    package: add fastfetch build rules

---
+++ package/fastfetch/fastfetch.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# fastfetch
+#
+################################################################################
+
+FASTFETCH_VERSION = 2.66.0
+FASTFETCH_SITE = https://github.com/fastfetch-cli/fastfetch/releases/download/$(FASTFETCH_VERSION)
+FASTFETCH_SOURCE = fastfetch-$(FASTFETCH_VERSION).tar.gz
+FASTFETCH_LICENSE = MIT
+FASTFETCH_LICENSE_FILES = LICENSE
+FASTFETCH_DEPENDENCIES = yyjson
+
+FASTFETCH_CONF_OPTS = \
+	-DENABLE_SYSTEM_YYJSON=ON \
+	-DBUILD_FLASHFETCH=OFF \
+	-DBUILD_TESTS=OFF \
+	-DPACKAGES_REMOVE_DISABLED=ON \
+	-DENABLE_DIRECTX_HEADERS=OFF \
+	-DENABLE_WAYLAND=OFF \
+	-DENABLE_XCB_RANDR=OFF \
+	-DENABLE_XRANDR=OFF \
+	-DENABLE_VULKAN=OFF \
+	-DENABLE_GIO=OFF \
+	-DENABLE_DCONF=OFF \
+	-DENABLE_DBUS=OFF \
+	-DENABLE_PULSE=OFF \
+	-DENABLE_DRM=OFF \
+	-DENABLE_DRM_AMDGPU=OFF
+
+$(eval $(cmake-package))
