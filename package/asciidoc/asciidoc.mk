2016ae8596a347a202528e037ebcef02c39b616e
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-08-15 22:05:56 +0200

    package: fix asciidoc source location

--- package/asciidoc/asciidoc.mk
+++ package/asciidoc/asciidoc.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 ASCIIDOC_VERSION = 10.2.1
-ASCIIDOC_SOURCE = asciidoc-$(ASCIIDOC_VERSION).tar.gz
-ASCIIDOC_SITE = https://files.pythonhosted.org/packages/1d/73/6d0a1f7f6e6c4f8b4b9f3c3c8b7a9f0a0f0a7a9e5e1d2c2f2f4d4e0e5e5e6
+ASCIIDOC_SOURCE = asciidoc-py-$(ASCIIDOC_VERSION).tar.gz
+ASCIIDOC_SITE = https://github.com/asciidoc-py/asciidoc-py/archive/refs/tags
 ASCIIDOC_LICENSE = GPL-2.0+
 ASCIIDOC_LICENSE_FILES = COPYING
 ASCIIDOC_SETUP_TYPE = setuptools
