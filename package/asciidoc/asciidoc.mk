0d4f38f71dce12bb3e628c6928ac728ede88d480
kuznix2011 <krzysztofdemirkuzniak@gmail.com>
2026-08-16 15:04:19 +0200

    fix asciidoc

--- package/asciidoc/asciidoc.mk
+++ package/asciidoc/asciidoc.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ASCIIDOC_VERSION = 10.2.1
-ASCIIDOC_SOURCE = v$(ASCIIDOC_VERSION).tar.gz
+ASCIIDOC_SOURCE = asciidoc-$(ASCIIDOC_VERSION).tar.gz
 ASCIIDOC_SITE = https://github.com/asciidoc-py/asciidoc-py/releases/download/$(ASCIIDOC_VERSION)
 ASCIIDOC_LICENSE = GPL-2.0+
 ASCIIDOC_LICENSE_FILES = COPYING
